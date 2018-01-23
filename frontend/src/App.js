f38c597ef7f09c5713a2edd81dd86cbd5d94ef69
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-01-23 11:53:12 -0600

    Add eslint and prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Data.world + Singer</h1>
-        <p>This app, allows you to configure and download an executable Singer pipeline.
-To get started:</p>
-        <button>Login with data.world</button>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <h1>Data.world + Singer</h1>
+    <p>
+      This app, allows you to configure and download an executable Singer
+      pipeline. To get started:
+    </p>
+    <button>Login with data.world</button>
+  </div>
+);
 
 export default App;
