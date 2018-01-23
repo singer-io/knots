bf202b6c7e12b9a60b6b101473012ef10b39c055
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-01-23 11:11:16 -0600

    Add home page text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Data.world + Singer</h1>
+        <p>This app, allows you to configure and download an executable Singer pipeline.
+To get started:</p>
+        <button>Login with data.world</button>
       </div>
     );
   }
