423794d6fc81b64823107b81f427953368c2fb51
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-01-24 15:01:56 -0600

    Implement OAuth

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { observer, inject } from 'mobx-react';
+import { Redirect } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import './Home.css';
 
-const Home = () => (
-  <div className="Home">
-    <h1>Data.world + Singer</h1>
-    <p>This app, allows you to configure and download an executable Singer</p>
-    <p>To get Started:</p>
-    <Link to="/taps">
-      <Button bsStyle="primary" className="login">
-        Login with data.world
-      </Button>
-    </Link>
-  </div>
-);
+const Home = (props) => {
+  if (props.userStore.token) {
+    return <Redirect push to="/taps" />;
+  }
+  return (
+    <div className="Home">
+      <h1>Data.world + Singer</h1>
+      <p>This app, allows you to configure and download an executable Singer</p>
+      <p>To get Started:</p>
+      <a href={process.env.REACT_APP_OAUTH_AUTH_URL}>
+        <Button bsStyle="primary" className="login">
+          Login with data.world
+        </Button>
+      </a>
+    </div>
+  );
+};
 
-export default Home;
+Home.propTypes = {
+  userStore: PropTypes.shape({
+    token: PropTypes.string
+  }).isRequired
+};
+
+export default inject('userStore')(observer(Home));
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './index.css';
+import { Provider } from 'mobx-react';
 import Home from './components/Home';
 import Taps from './components/Taps';
 import Connection from './components/Connection';
 import Schema from './components/Schema';
 import Dataworld from './components/Dataworld';
 import Finish from './components/Finish';
+import Callback from './components/Callback';
 import registerServiceWorker from './registerServiceWorker';
+import usersStore from './stores/user';
+
+import './index.css';
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/taps" component={Taps} />
-      <Route path="/connection" component={Connection} />
-      <Route path="/schema" component={Schema} />
-      <Route path="/dataworld" component={Dataworld} />
-      <Route path="/finish" component={Finish} />
-    </div>
-  </Router>,
+  <Provider userStore={usersStore}>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/taps" component={Taps} />
+        <Route path="/connection" component={Connection} />
+        <Route path="/schema" component={Schema} />
+        <Route path="/dataworld" component={Dataworld} />
+        <Route path="/finish" component={Finish} />
+        <Route path="/callback" component={Callback} />
+      </div>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
