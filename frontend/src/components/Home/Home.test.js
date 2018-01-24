afcb2249ee4d2ce74312b842ad44380ebb0fc99e
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-01-24 10:31:52 -0600

    Increase login button's top margin

--- frontend/src/components/Home/Home.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './index';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -3,15 +3,17 @@ import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
-const App = () => (
-  <div className="App">
+const Home = () => (
+  <div className="Home">
     <h1>Data.world + Singer</h1>
     <p>This app, allows you to configure and download an executable Singer</p>
     <p>To get Started:</p>
     <Link to="/taps">
-      <Button bsStyle="primary">Login with data.world</Button>
+      <Button bsStyle="primary" className="login">
+        Login with data.world
+      </Button>
     </Link>
   </div>
 );
 
-export default App;
+export default Home;
