daba6259d755d2e4184ab8a89b60bc5378a1f6e9
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-02-19 15:20:33 +0300

    [#23] Add eslint and Prettier

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Reacts</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => <div className="App">Hello There</div>;
 
 export default App;
