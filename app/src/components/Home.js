f87db62c493f4f34ac054cfdb00f4ff56c515d69
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-02-20 16:37:31 +0300

    Add home page

---
+++ app/src/components/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = () => <div className="App">Home</div>;
+
+export default Home;
--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'mobx-react';
+import Home from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
