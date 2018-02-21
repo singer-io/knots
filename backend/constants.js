ecf6e0d1f4a422102a3162f9c3ba1434c37a82b0
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-02-21 14:27:58 +0300

    [#30] Add taps page (#45)

---
+++ backend/constants.js
@@ -0,0 +1,47 @@
+const activeTaps = [
+  {
+    name: 'Redshift',
+    key: 'tap-redshift',
+    repo: 'https://github.com/datadotworld/tap-redshift',
+    logo:
+      'https://cdn.zapier.com/storage/services/1e66b95901e0564c9e990c320705b69a.128x128.png'
+  }
+];
+
+const inactiveTaps = [
+  {
+    name: 'Redshift',
+    key: 'tap-redshift',
+    repo: 'https://github.com/datadotworld/tap-redshift',
+    logo:
+      'https://cdn.zapier.com/storage/services/1e66b95901e0564c9e990c320705b69a.128x128.png'
+  },
+  {
+    name: 'Redshift',
+    key: 'tap-redshift',
+    repo: 'https://github.com/datadotworld/tap-redshift',
+    logo:
+      'https://cdn.zapier.com/storage/services/1e66b95901e0564c9e990c320705b69a.128x128.png'
+  },
+  {
+    name: 'Redshift',
+    key: 'tap-redshift',
+    repo: 'https://github.com/datadotworld/tap-redshift',
+    logo:
+      'https://cdn.zapier.com/storage/services/1e66b95901e0564c9e990c320705b69a.128x128.png'
+  },
+  {
+    name: 'Redshift',
+    key: 'tap-redshift',
+    repo: 'https://github.com/datadotworld/tap-redshift',
+    logo:
+      'https://cdn.zapier.com/storage/services/1e66b95901e0564c9e990c320705b69a.128x128.png'
+  }
+];
+
+module.exports = {
+  taps: {
+    activeTaps,
+    inactiveTaps
+  }
+};
