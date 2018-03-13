454df47d102c142756351d3d98b4c3212c757e34
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-03-13 17:35:52 +0300

    Turn dataset field into a dropdown

--- app/src/components/ConnectTarget/DataWorld/index.js
+++ app/src/components/ConnectTarget/DataWorld/index.js
@@ -1,43 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { observer, inject } from 'mobx-react';
-import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import { Button, FormControl, ControlLabel } from 'react-bootstrap';
 
 import './DataWorld.css';
 
-const DataWorld = (props) => (
-  <div className="DataWorld">
-    {props.userStore.token && (
-      <div>
-        <ControlLabel className="control-label">API Token</ControlLabel>
-        <FormControl
-          name="dataset"
-          type="text"
-          value={props.userStore.token}
-          readOnly
-        />
+class DataWorld extends Component {
+  componentWillMount() {
+    if (this.props.userStore.token) {
+      this.props.userStore.getDatasets();
+    }
+  }
+  render() {
+    return (
+      <div className="DataWorld">
+        {this.props.userStore.token && (
+          <div>
+            <ControlLabel className="control-label">API Token</ControlLabel>
+            <FormControl
+              name="dataset"
+              type="text"
+              value={this.props.userStore.token}
+              readOnly
+            />
+          </div>
+        )}
+        {!this.props.userStore.token && (
+          <div>
+            <div className="button-label">API Token</div>
+            <a href="https://data.world/oauth/authorize?client_id=knot-local&redirect_uri=http://localhost:3000/callback&response_type=code">
+              <Button bsStyle="primary">Log in with data.world</Button>
+            </a>
+          </div>
+        )}
+        <div>
+          <ControlLabel>Dataset</ControlLabel>
+          <select onChange={this.handleChange}>
+            {this.props.userStore.datasets.map((dataset) => (
+              <option key={dataset} value={dataset}>
+                {dataset}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-    )}
-    {!props.userStore.token && (
-      <div>
-        <div className="button-label">API Token</div>
-        <a href="https://data.world/oauth/authorize?client_id=knot-local&redirect_uri=http://localhost:3000/callback&response_type=code">
-          <Button bsStyle="primary">Log in with data.world</Button>
-        </a>
-      </div>
-    )}
-    <FormGroup controlId="formBasicText">
-      <div>
-        <ControlLabel className="control-label">Dataset</ControlLabel>
-        <FormControl name="dataset" type="text" onChange={this.handleChange} />
-      </div>
-    </FormGroup>
-  </div>
-);
+    );
+  }
+}
 
 DataWorld.propTypes = {
   userStore: PropTypes.shape({
-    token: PropTypes.string
+    token: PropTypes.string,
+    getDatasets: PropTypes.func,
+    datasets: PropTypes.object
   }).isRequired
 };
 
