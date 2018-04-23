c4a3116f606dfc25be62635b459eeddd7e8e3afb
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-04-23 14:13:26 +0300

    Use components on home page

--- app/components/Home/Create/index.js
+++ app/components/Home/Create/index.js
@@ -1,14 +1,14 @@
 // @flow
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron } from 'reactstrap';
+import { Container, Jumbotron, Button } from 'reactstrap';
 
 import Header from '../../Header';
 
 const Create = () => (
   <div>
     <Header />
-    <div className="container mt-5">
+    <Container className="mt-5">
       <Jumbotron>
         <h1 className="display-4">
           There is <em>knothing</em> here!
@@ -17,11 +17,13 @@ const Create = () => (
           This app allows you to configure and download executable Singer
           pipelines.
         </p>
-        <Link className="btn btn-primary btn-lg" to="/taps" role="button">
-          Get Started
+        <Link to="/taps">
+          <Button color="primary" size="lg">
+            Get Started
+          </Button>
         </Link>
       </Jumbotron>
-    </div>
+    </Container>
   </div>
 );
 
--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -2,33 +2,26 @@
 import React, { Component } from 'react';
 
 import Create from './Create';
-import Knots from './Knots';
+// import Knots from './Knots';
 
-type Props = {
-  fetchKnots: () => void,
-  knotsStore: { knots: Array<string> },
-  download: (name: string) => void,
-  syncMode: (name: string) => void
-};
+// type Props = {
+//   fetchKnots: () => void,
+//   knotsStore: { knots: Array<string> },
+//   download: (name: string) => void,
+//   syncMode: (name: string) => void
+// };
 
-export default class Home extends Component<Props> {
-  componentWillMount() {
-    this.props.fetchKnots();
-  }
+export default class Home extends Component<{}> {
+  // componentWillMount() {
+  //   this.props.fetchKnots();
+  // }
 
   render() {
-    console.log('THe props');
-    if (this.props.knotsStore) {
-      if (this.props.knotsStore.knots.length > 0) {
-        return (
-          <Knots
-            knots={this.props.knotsStore.knots}
-            download={this.props.download}
-            syncMode={this.props.syncMode}
-          />
-        );
-      }
-    }
+    // if (this.props.knotsStore) {
+    //   if (this.props.knotsStore.knots.length > 0) {
+    //     return <div>Knots will appear here</div>;
+    //   }
+    // }
 
     return <Create />;
   }
