26889b36a676f78c6b155be72188a64af20cddc8
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-04-25 17:35:14 +0300

    Home page

--- app/components/Home/Create/index.js
+++ app/components/Home/Create/index.js
@@ -1,52 +1,16 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Jumbotron, Button, Alert } from 'reactstrap';
+import { Container, Jumbotron, Button } from 'reactstrap';
 
 import Header from '../../Header';
 
-type Props = {
-  showAlert: boolean
-};
-
-type State = {
-  visible: boolean
-};
-
-class Create extends Component<Props, State> {
-  constructor() {
-    super();
-    this.state = { visible: false };
-    this.onDismiss = this.onDismiss.bind(this);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState({ visible: nextProps.showAlert });
-  }
-
-  onDismiss() {
-    this.setState({ visible: false });
-  }
-
+class Create extends Component<{}> {
   render() {
     return (
       <div>
         <Header />
         <Container className="mt-5">
-          <Alert
-            color="danger"
-            style={{ opacity: 1 }}
-            isOpen={this.state.visible}
-            toggle={this.onDismiss}
-          >
-            Oops! Docker must be installed before you can proceed.<a
-              className="alert-link"
-              href="https://store.docker.com/editions/community/docker-ce-desktop-mac"
-              target="_target"
-            >
-              Click here to download it.
-            </a>
-          </Alert>
           <Jumbotron>
             <h1 className="display-4">
               There is <em>knothing</em> here!
--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -2,27 +2,9 @@
 import React, { Component } from 'react';
 
 import Create from './Create';
-// import Knots from './Knots';
 
-type Props = {
-  fetchKnots: () => void,
-  knotsStore: { knots: Array<string>, showAlert: boolean }
-  // download: (name: string) => void,
-  // syncMode: (name: string) => void
-};
-
-export default class Home extends Component<Props> {
-  componentWillMount() {
-    this.props.fetchKnots();
-  }
-
+export default class Home extends Component<{}> {
   render() {
-    // if (this.props.knotsStore) {
-    //   if (this.props.knotsStore.knots.length > 0) {
-    //     return <div>Knots will appear here</div>;
-    //   }
-    // }
-
-    return <Create showAlert={this.props.knotsStore.showAlert} />;
+    return <Create />;
   }
 }
