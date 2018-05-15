e62e20561327fda60d44743d141bc81b1c7604fe
andela-gokerayi <gbolahan.okerayi@andela.com>
2018-05-15 17:20:31 +0100

    Rename Knot to Knots

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Header = () => (
   <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-    <h5 className="my-0 mr-md-auto font-weight-normal">Knot</h5>
+    <h5 className="my-0 mr-md-auto font-weight-normal">Knots</h5>
     <nav className="my-2 my-md-0 mr-md-3">
       <a className="p-2 text-dark" href="#">
         My Knots
