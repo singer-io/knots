4cb61e5a89288b340f4a91ca40eef45853e28d9c
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-05-25 15:28:48 +0300

    Fix link on pacakged app

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,13 +1,14 @@
 // @flow
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
     <h5 className="my-0 mr-md-auto font-weight-normal">Knots</h5>
     <nav className="my-2 my-md-0 mr-md-3">
-      <a className="p-2 text-dark" href="#">
+      <Link to="/" className="p-2 text-dark">
         My Knots
-      </a>
+      </Link>
     </nav>
   </div>
 );
