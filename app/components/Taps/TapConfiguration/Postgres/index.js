43515a39cc16d57eb635b611a0665ba11cb9e43e
andela-gokerayi <gbolahan.okerayi@andela.com>
2018-06-29 12:43:38 +0100

    Validate hostname input

--- app/components/Taps/TapConfiguration/Postgres/index.js
+++ app/components/Taps/TapConfiguration/Postgres/index.js
@@ -74,6 +74,15 @@ export default class Postgres extends Component<Props, State> {
     }
   };
 
+  validateHostName = (value: string) => {
+    const regex = /^[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,}$/gm;
+    if (regex.test(value)) {
+      this.setState({ host: { valid: true } });
+    } else {
+      this.setState({ host: { invalid: true } });
+    }
+  };
+
   handleChange = (e: SyntheticEvent<HTMLButtonElement>) => {
     const { name } = e.currentTarget;
     let { value } = e.currentTarget;
@@ -105,9 +114,8 @@ export default class Postgres extends Component<Props, State> {
                   onFocus={() => {
                     this.setState({ host: {} });
                   }}
-                  onBlur={(event) => {
-                    const { value } = event.currentTarget;
-                    this.validate('host', value);
+                  onBlur={() => {
+                    this.validateHostName(host);
                   }}
                   onChange={this.handleChange}
                   {...this.state.host}
