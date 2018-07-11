b551841c8abe0b80d397f7afe427016b5cd14a6c
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2018-07-11 15:08:36 +0300

    Test sync command

--- test/backend/commands.spec.js
+++ test/backend/commands.spec.js
@@ -1,9 +1,13 @@
+import path from 'path';
 import { commands } from '../../app/backend/constants';
 
 describe('commands', () => {
   it('returns the correct discovery command', () => {
-    const expected =
-      'docker run -v "applicationFolder/tap:/app/tap-redshift/data" dataworld/tap-redshift:1.0.0b8 tap-redshift -c tap-redshift/data/config.json -d > "applicationFolder/tap/catalog.json"';
+    const expected = `docker run -v "${path.resolve(
+      'applicationFolder'
+    )}/tap:/app/tap-redshift/data" dataworld/tap-redshift:1.0.0b8 tap-redshift -c tap-redshift/data/config.json -d > "${path.resolve(
+      'applicationFolder'
+    )}/tap/catalog.json"`;
 
     const actual = commands.runDiscovery(
       'applicationFolder',
@@ -11,6 +15,58 @@ describe('commands', () => {
       'dataworld/tap-redshift:1.0.0b8'
     );
 
-    expect(expected).toEqual(actual);
+    expect(actual).toEqual(expected);
+  });
+
+  it('returns the correct sync command (uses catalog)', () => {
+    const expected = `docker run -v "${path.resolve(
+      'applicationFolder'
+    )}/tap:/app/tap-redshift/data" --interactive dataworld/tap-redshift:1.0.0b8 tap-redshift -c tap-redshift/data/config.json --catalog tap-redshift/data/catalog.json 2> "${path.resolve(
+      'applicationFolder'
+    )}/tap.log" | docker run -v "${path.resolve(
+      'applicationFolder'
+    )}/target:/app/target-datadotworld/data" --interactive dataworld/target-datadotworld:1.0.1 target-datadotworld -c target-datadotworld/data/config.json 2> "${path.resolve(
+      'applicationFolder'
+    )}/target.log" > "${path.resolve('applicationFolder')}/tap/state.json"`;
+
+    const actual = commands.runSync(
+      'applicationFolder',
+      { name: 'tap-redshift', image: 'dataworld/tap-redshift:1.0.0b8' },
+      {
+        name: 'target-datadotworld',
+        image: 'dataworld/target-datadotworld:1.0.1'
+      }
+    );
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('returns the correct sync command (uses properties)', () => {
+    const expected = `docker run -v "${path.resolve(
+      'applicationFolder'
+    )}/tap:/app/tap-redshift/data" --interactive dataworld/tap-redshift:1.0.0b8 tap-redshift -c tap-redshift/data/config.json --properties tap-redshift/data/catalog.json 2> "${path.resolve(
+      'applicationFolder'
+    )}/tap.log" | docker run -v "${path.resolve(
+      'applicationFolder'
+    )}/target:/app/target-datadotworld/data" --interactive dataworld/target-datadotworld:1.0.1 target-datadotworld -c target-datadotworld/data/config.json 2> "${path.resolve(
+      'applicationFolder'
+    )}/target.log" > "${path.resolve('applicationFolder')}/tap/state.json"`;
+
+    const actual = commands.runSync(
+      'applicationFolder',
+      {
+        name: 'tap-redshift',
+        image: 'dataworld/tap-redshift:1.0.0b8',
+        specImplementation: {
+          usesCatalogArg: false
+        }
+      },
+      {
+        name: 'target-datadotworld',
+        image: 'dataworld/target-datadotworld:1.0.1'
+      }
+    );
+
+    expect(actual).toEqual(expected);
   });
 });
