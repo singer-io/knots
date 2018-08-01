40abc92c1c40aa12c4b1b398b62f6ee29a09c1f2
andela-gokerayi <gbolahan.okerayi@andela.com>
2018-08-01 14:15:20 +0100

    Use react select for key props multi select input

---
+++ app/containers/S3.js
@@ -0,0 +1,39 @@
+/*
+ * knots
+ * Copyright 2018 data.world, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ * This product includes software developed at
+ * data.world, Inc.(http://data.world/).
+ */
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as TapsActions from '../actions/taps';
+import S3 from '../components/Taps/TapConfiguration/S3';
+
+function mapStateToProps(state) {
+  return { tapsStore: state.taps };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(TapsActions, dispatch);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(S3);
